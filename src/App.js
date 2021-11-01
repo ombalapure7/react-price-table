10cabf3320be898603d3e22326da2e144b7b060b
ombalapure7 <65531892+ombalapure7@users.noreply.github.com>
2021-11-01 22:46:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import CardContainer from "./components/CardContainer";
+import Jumbotron from "./components/Jumbotron";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Jumbotron />
+      <CardContainer />
+    </div>
+  );
+}
+
+export default App;
